5d8036c0ced3e6ab9959c4bda55b60ba3b811af9
Shota Minegishi <smngs@mine-313.com>
2022-11-08 20:33:20 +0900

    [update] add tweet scheme & test response

--- routers/tweet.py
+++ routers/tweet.py
@@ -1,11 +1,26 @@
 from fastapi import APIRouter
 
+from typing import List
+import datetime
+import schemas.tweet as tweet_schema
+
 router = APIRouter()
 
 # Tweet
 @router.get("/tweets", tags=["tweets"])
 async def list_tweets():
-    pass
+    return [
+        tweet_schema.Tweet(
+            tweet_id=1, 
+            name='Shota Minegishi', 
+            userName='@smngs', 
+            imageURL="https://yahoo.co.jp",
+            createdAt=datetime.datetime.now(),
+            favorite={
+                "isFavorite": False,
+                "count": 30
+            }
+        )]
 
 @router.get("/tweets/{tweet_id}", tags=["tweets"])
 async def get_tweet():
