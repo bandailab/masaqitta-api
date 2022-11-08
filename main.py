8ca0a63ea0aebd2c23a3606a3cbf9e4d14c12d46
Shota Minegishi <smngs@mine-313.com>
2022-11-08 17:36:34 +0900

    [update] add dummyuser.json && add endpoint `/user`

--- main.py
+++ main.py
@@ -2,8 +2,12 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
 import json
-json_open = open('dummytweet.json', 'r')
-json_load = json.load(json_open)
+
+tweet_json_open = open('dummytweet.json', 'r')
+tweet_json_load = json.load(tweet_json_open)
+
+user_json_open = open('dummyuser.json', 'r')
+user_json_load = json.load(user_json_open)
 
 app = FastAPI()
 
@@ -25,5 +29,8 @@ def read_root():
 
 @app.get("/tweets")
 def get_tweets():
-    return json_load
+    return tweet_json_load
 
+@app.get("/user")
+def get_user():
+    return user_json_load 
